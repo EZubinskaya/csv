299bbdb2e772ed8d361aa40070ae560cb2e016d6
ekaterina <k.zubinska@comparus.de>
2017-10-02 14:57:19 +0300

    sdf
    sdfs

--- einlagensicherungsgesetz/src/main/java/comparus/de/tasks/Task1a.java
+++ einlagensicherungsgesetz/src/main/java/comparus/de/tasks/Task1a.java
@@ -28,7 +28,7 @@ public class Task1a {
         for(int i = 1; i < myEntries.size(); i++) {
             //starts with A2
             String[] curEl = myEntries.get(i)[0].split(";", -1);
-            String key = curEl[3];
+            String key = curEl[2] + curEl[3];
             if(curEl[3].startsWith("GEMKD") && curEl[29].equals("J")) {
                 specialCRecords(C, curEl);
             } else {
@@ -131,8 +131,8 @@ public class Task1a {
         valueZusatz002.setC2B(newC2B_Zusatz002 + "-" + curEl[0]);
         valueZusatz003.setC2B(newC2B_Zusatz003 + "-" + curEl[0]);
 
-        C.put(valueZusatz002.getC2B(), valueZusatz002);
-        C.put(valueZusatz003.getC2B(), valueZusatz003);
+        C.put(valueZusatz002.getC2A() + valueZusatz002.getC2B(), valueZusatz002);
+        C.put(valueZusatz002.getC2A() + valueZusatz003.getC2B(), valueZusatz003);
     }
 
     public static void reCalculateD_task1A(Map<String, CVSClient> fullFile, String[] a, List<String> b) {
